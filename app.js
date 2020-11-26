1b8e7e8f65b7d00e003fdea5d623fc5ef89f8b5f
erickakoyama <ericka.koyama@gmail.com>
2020-11-26 06:05:01 +0000

    wip

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+
+// app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// routes
+app.get('/', async(req, res) => {
+  const apiUrl = 'https://api.unsplash.com/photos/random/?client_id=ZHgmuOowRNnzY-nwFEKK1xBCqeOD9H_bfNpAXnnlUvI&featured=true';
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  res.render('index', { imageUrl: data.urls.small });
+});
+
+// starting server
+app.listen(process.env.PORT, process.env.IP, () => {
+  console.log('Express server is running...');
+});
