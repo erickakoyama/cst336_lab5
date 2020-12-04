209a4b4c60b7e0bdc87671e8b615c7e0573eee70
erickakoyama <ericka.koyama@gmail.com>
2020-12-04 04:13:29 +0000

    wip

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
+const pool = require('./dbPool.js');
+
+require('dotenv').config(); // process.env variables
 
 // app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
@@ -26,6 +29,28 @@ app.get('/search', async(req, res) => {
 
   res.render('results', { imageUrl: bgImage, imageUrlArray });
 });
+
+app.get("/api/updateFavorites", function(req, res) {
+  let sql;
+  let sqlParams;
+  switch (req.query.action) {
+    case "add":
+      sql = "INSERT INTO favorites (imageUrl, keyword) VALUES (?,?)";
+      sqlParams = [req.query.imageUrl, req.query.keyword];
+      break;
+    case "delete":
+      sql = "DELETE FROM favorites WHERE imageUrl = ?";
+      sqlParams = [req.query.imageUrl];
+      break;
+  } //switch
+  pool.query(sql, sqlParams, function(err, rows, fields) {
+    if (err) throw err;
+    console.log(rows);
+    res.send(rows.affectedRows.toString());
+  });
+
+}); //api/updateFavorites
+
 
 // starting server
 app.listen(process.env.PORT, process.env.IP, () => {
--- public/js/functions.js
+++ public/js/functions.js
@@ -1,12 +1,26 @@
+const updateFavorite = async(action, imageUrl, keyword) => {
+  const url = `/api/updateFavorites?action=${action}&imageUrl=${imageUrl}&keyword=${keyword}`;
+  await fetch(url);
+}
+
 const favoriteIconNodes = document.querySelectorAll('.favoriteIcon');
 
 Array.from(favoriteIconNodes).map(node => {
   const handleFavoriteClick = () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const keyword = urlParams.get("keyword");
+
+    const imageUrl = $(this).prev().attr("src");
+
+
     if (node.getAttribute('src') === 'img/favorite.png') {
       node.setAttribute('src', 'img/favorite_on.png');
+      updateFavorite('add', imageUrl, keyword);
     }
     else {
       node.setAttribute('src', 'img/favorite.png');
+      updateFavorite('delete', imageUrl);
     }
   };
 
